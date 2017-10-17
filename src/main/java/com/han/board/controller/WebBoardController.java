fd289d776cd8162419ce72b1b92d5d03a709f92b
bakgaboon <bakgaboon@gmail.com>
2017-10-17 18:06:26 +0900

    p306

--- src/main/java/com/han/board/controller/WebBoardController.java
+++ src/main/java/com/han/board/controller/WebBoardController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,11 +37,12 @@ public class WebBoardController {
 //    }
 
     @GetMapping("/list")
-    public void list(PageVO vo, Model model) {
+    public void list(@ModelAttribute("pageVO") PageVO vo, Model model) {
 
         Pageable page = vo.makePageable(0, "bno");
 
-        Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), page);
+        Page<WebBoard> result = repo.findAll(
+                repo.makePredicate(vo.getType(), vo.getKeyword()), page);
 
         log.info("" + page);
         log.info("" + result);
