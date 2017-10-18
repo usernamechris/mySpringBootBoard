bcfde97bf3f655540a85d1a0061f403edbeea0e7
bakgaboon <bakgaboon@gmail.com>
2017-10-18 17:17:43 +0900

    p308

--- src/main/java/com/han/board/controller/WebBoardController.java
+++ src/main/java/com/han/board/controller/WebBoardController.java
@@ -14,7 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/boards/")
@@ -51,5 +53,36 @@ public class WebBoardController {
 
         model.addAttribute("result", new PageMaker(result));
 
+    }
+
+    @GetMapping("/register")
+    public void registerGET(@ModelAttribute("vo") WebBoard vo) {
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+        log.info("register post");
+        log.info("" + vo);
+
+        repo.save(vo);
+        rttr.addFlashAttribute("msg", "success");
+
+        return "redirect:/boards/list";
+    }
+
+    @GetMapping("/view")
+    public void view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+        log.info("BNO" + bno);
+
+        repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+
+    }
+
+    @GetMapping("/modify")
+    public void modify(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model) {
+        log.info("Modify bno: " + bno);
+
+        repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
     }
 }
